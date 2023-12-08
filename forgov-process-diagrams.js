9308c3c7d19ffa02c082a2efba771d47130d34ff
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2023-12-08 14:57:05 +1000

    initial support for directionality and connectors

--- forgov-process-diagrams.js
+++ forgov-process-diagrams.js
@@ -2,8 +2,10 @@ const fgProcessDiagram = () => {
   let processDiagram = document.querySelectorAll('.fg_process-diagram')
 
   processDiagram.forEach((diagram) => {
-    diagram.querySelectorAll('li').forEach((item) => {
+    let diagram_nodes = diagram.querySelectorAll('li')
+    diagram_nodes.forEach((item) => {
       let text = item.firstChild && item.firstChild.nodeType === Node.TEXT_NODE ? item.firstChild.textContent.trim() : false,
+      // TODO: refactor to accept HTML content (paragraphs, anchors)
         style_classes = ['success', 'warning', 'danger', 'info'],
         item_style = [...item.classList].filter((item_class) => {
           return style_classes.includes(item_class)
@@ -25,8 +27,115 @@ const fgProcessDiagram = () => {
         item.replaceChild(card, item.firstChild)
       }
 
+
+    })
+    diagram_nodes.forEach((item) => {
+
+      if (item.dataset.connectsTo) {
+        fgProcessDiagramConnect(item, diagram.querySelector(`#${item.dataset.connectsTo}`))
+      }
     })
   })
 }
+
+const fgProcessDiagramConnect = (current, target) => {
+  console.log(current, target);
+  let connector = document.createElement('DIV'),
+    current_position = current.getBoundingClientRect(),
+    target_position = target.getBoundingClientRect(),
+    target_left = current_position.x > target_position.x ? true : false,
+    target_above = current_position.y > target_position.y ? true : false,
+    target_card = target.querySelector('.card'),
+    target_card_position = target_card.getBoundingClientRect()
+
+  connector.classList.add('connector')
+  console.log('current:', current_position)
+  console.log('target:', target_position)
+  console.log('target card:', target_card_position)
+
+  let target_connector = { // defaults
+    height: 10,
+    width: 10
+  }
+  if (target_left) {
+    connector.classList.add('connect-left')
+
+      // target_connector.left = (targetPosition.right - targetPosition.left) / 2
+      target_connector.left = target_position.width / 2
+      target_connector.width = (target_position.width / 2) + (current_position.left - target_position.right)
+
+    console.log('target left')
+    console.log({
+      target_width: target_position.width
+    })
+
+    if (target_above) {
+      console.log('target above')
+      connector.classList.add('connect-above')
+
+      target_connector.top = target_position.height
+      target_connector.height = (current_position.top - target_position.bottom) + (current_position.height / 2)
+    }
+    else if (target_position.y === current_position.y) {
+      connector.classList.add('connect-level')
+      target_connector.width = current_position.left - target_position.right
+      target_connector.right = target_connector.width
+      target_connector.left = target_position.width
+      target_connector.top = current_position.height / 2
+    }
+    else {
+      connector.classList.add('connect-below')
+      console.log('target below')
+    }
+  }
+  else { // targetRight
+    connector.classList.add('connect-right')
+    console.log('target right')
+
+    // targetRight connector needs to be offset 2px because of...border width?
+    target_connector.right = target_position.width / 2 - 2
+    target_connector.width = (target_position.width / 2) + (target_position.left - current_position.right) + 2
+
+    if (target_above) {
+      console.log('target above')
+      connector.classList.add('connect-above')
+
+      target_connector.top= target_position.height
+      target_connector.height = (current_position.top - target_position.bottom) + (current_position.height / 2)
+    }
+    else if (target_position.y === current_position.y) {
+      connector.classList.add('connect-level')
+      target_connector.width = target_position.left - current_position.right
+      target_connector.left = -target_connector.width
+      target_connector.top = current_position.height / 2
+    }
+    else {
+      console.log('target below')
+      connector.classList.add('connect-below')
+
+      target_connector.top = 0;
+      target_connector.height = (target_card_position.top - current_position.bottom) + target_card_position.height / 2
+      target_connector.right = (target_position.width / 2) + (current_position.width / 2)
+      target_connector.width = (current_position.width / 2) + (target_card_position.left - current_position.right)
+    }
+
+  }
+
+  // connector.style = `
+  // height: ${target_connector.height}px;
+  // width: ${target_connector.width}px;
+  // top: ${target_connector.top}px;
+  // left: ${target_connector.left}px;
+  // right: ${target_connector.right}px;
+  // `
+
+  connector.style = Object.keys(target_connector).map((property) => {
+    return `${property}: ${target_connector[property]}px;`
+  }).join('')
+
+
+  target.appendChild(connector)
+}
+
 
 fgProcessDiagram();
