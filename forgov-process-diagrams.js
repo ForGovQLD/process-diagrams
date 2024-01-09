b5266a33f5612df92425ce5c86f2442a68dc1584
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2024-01-09 16:02:00 +1000

    tidy up

--- forgov-process-diagrams.js
+++ forgov-process-diagrams.js
@@ -16,15 +16,15 @@ const fgProcessDiagram = () => {
             return node.outerHTML
           }
           }),
-        style_classes = ['success', 'warning', 'danger', 'info'],
-        item_style = [...item.classList].filter((item_class) => {
-          return style_classes.includes(item_class)
-        })
+          style_classes = ['success', 'warning', 'danger', 'info'],
+          item_style = [...item.classList].filter((item_class) => {
+            return style_classes.includes(item_class)
+          })
 
       // Wrap content in div tags and turn into card
       if (content) {
         let card = document.createElement('DIV'),
-          card_body = document.createElement('DIV')
+            card_body = document.createElement('DIV')
 
         card_body.innerHTML = content.join('')
         card_body.classList.add('card-body')
@@ -53,44 +53,40 @@ const fgProcessDiagram = () => {
 }
 
 const fgProcessDiagramConnect = (current, target) => {
-  console.log(current, target);
+  // TODO: D.R.Y. out this function
+  // console.log(current, target)
   current.setAttribute('aria-flowto',target.id)
 
   let connector = document.createElement('DIV'),
-    current_position = current.getBoundingClientRect(),
-    target_position = target.getBoundingClientRect(),
-    target_above = current_position.y > target_position.y ? true : false,
-    target_card = target.querySelector('.card'),
-    target_card_position = target_card.getBoundingClientRect()
+      current_position = current.getBoundingClientRect(),
+      target_position = target.getBoundingClientRect(),
+      target_above = current_position.y > target_position.y ? true : false,
+      target_card = target.querySelector('.card'),
+      target_card_position = target_card.getBoundingClientRect()
 
   connector.classList.add('connector')
-  console.log('current:', current_position)
-  console.log('target:', target_position)
-  console.log('target card:', target_card_position)
+
+  // console.log('current:', current_position)
+  // console.log('target:', target_position)
+  // console.log('target card:', target_card_position)
 
   let target_connector = { // defaults
-    height: 10,
-    width: 10
-  }
-
-    let current_center = current_position.x + (current_position.width / 2),
-        target_card_center = target_card_position.x + (target_card_position.width / 2),
-        target_left = current_center > target_card_center ? true : false
+        height: 10,
+        width: 10
+      },
+      current_center = current_position.x + (current_position.width / 2),
+      target_card_center = target_card_position.x + (target_card_position.width / 2),
+      target_left = current_center > target_card_center ? true : false
 
   if (current_center > (target_card_center - 15)
       && current_center < (target_card_center + 15)) {
-    console.log('TARGET CENTRE')
     connector.classList.add('connect-center')
 
     target_connector.left = target_position.width / 2
 
     if (target_above) {
+      // TODO: Is this scenario ever possible? Would be connecting back to the card directly above...
       connector.classList.add('connect-above')
-
-    }
-    else if (target_position.y === current_position.y) {
-      connector.classList.add('connect-level')
-
     }
     else {
       connector.classList.add('connect-below')
@@ -102,17 +98,10 @@ const fgProcessDiagramConnect = (current, target) => {
   else if (target_left) {
     connector.classList.add('connect-left')
 
-      // target_connector.left = (targetPosition.right - targetPosition.left) / 2
       target_connector.left = target_position.width / 2
       target_connector.width = (target_position.width / 2) + (current_position.left - target_position.right)
 
-    console.log('target left')
-    console.log({
-      target_width: target_position.width
-    })
-
     if (target_above) {
-      console.log('target above')
       connector.classList.add('connect-above')
 
       target_connector.top = target_position.height
@@ -127,26 +116,21 @@ const fgProcessDiagramConnect = (current, target) => {
     }
     else {
       connector.classList.add('connect-below')
-      console.log('target below')
-
       target.classList.add('connect-below-target')
 
       target_connector.top = current_position.bottom - target_card_position.top
       target_connector.height = (target_card_position.top - current_position.bottom)
       target_connector.width = target_connector.width + (current_position.width / 2)
-      // target_connector.right = current_position.width / 2
     }
   }
-  else { // targetRight
+  else { // target_right
     connector.classList.add('connect-right')
-    console.log('target right')
 
-    // targetRight connector needs to be offset 2px because of...border width?
+    // target_right connector needs to be offset 2px because of...border width?
     target_connector.right = target_position.width / 2 - 2
     target_connector.width = (target_position.width / 2) + (target_position.left - current_position.right) + 2
 
     if (target_above) {
-      console.log('target above')
       connector.classList.add('connect-above')
 
       target_connector.top= target_position.height
@@ -159,7 +143,6 @@ const fgProcessDiagramConnect = (current, target) => {
       target_connector.top = current_position.height / 2
     }
     else {
-      console.log('target below')
       connector.classList.add('connect-below')
 
       target_connector.top = current_position.bottom - target_card_position.top
