23920e2d8502659bff3f454c56a24572664c64ec
Jordan Quigg <jordan.quigg@chde.qld.gov.au>
2024-01-09 16:01:26 +1000

    support complex content in cards

--- forgov-process-diagrams.js
+++ forgov-process-diagrams.js
@@ -3,34 +3,48 @@ const fgProcessDiagram = () => {
 
   processDiagram.forEach((diagram) => {
     let diagram_nodes = diagram.querySelectorAll('li')
+
     diagram_nodes.forEach((item) => {
-      let text = item.firstChild && item.firstChild.nodeType === Node.TEXT_NODE ? item.firstChild.textContent.trim() : false,
-      // TODO: refactor to accept HTML content (paragraphs, anchors)
+      let childNodes = [...item.childNodes].filter((node) => {
+            return node.nodeName !== "OL"
+          }),
+          content = childNodes.map((node) => {
+          if (node.nodeType === 3) { // 3 = Node.TEXT_NODE
+            return node.textContent
+          }
+          else if (node.nodeType === 1) { // 1 = Node.ELEMENT_NODE
+            return node.outerHTML
+          }
+          }),
         style_classes = ['success', 'warning', 'danger', 'info'],
         item_style = [...item.classList].filter((item_class) => {
           return style_classes.includes(item_class)
         })
 
-      // Wrap text in div tags and turn into card
-      if (text) {
+      // Wrap content in div tags and turn into card
+      if (content) {
         let card = document.createElement('DIV'),
           card_body = document.createElement('DIV')
 
-        card_body.innerText = text
+        card_body.innerHTML = content.join('')
         card_body.classList.add('card-body')
         card.classList.add('card')
+
         if (item_style.length) {
           card.classList.add('card-' + item_style[0])
           item.classList.remove(item_style[0])
         }
+
         card.appendChild(card_body)
         item.replaceChild(card, item.firstChild)
+        childNodes.shift() // first child node has been replaced above
+        childNodes.forEach((node) => {
+          item.removeChild(node)
+        })
       }
-
-
     })
-    diagram_nodes.forEach((item) => {
 
+    diagram_nodes.forEach((item) => {
       if (item.dataset.connectsTo) {
         fgProcessDiagramConnect(item, diagram.querySelector(`#${item.dataset.connectsTo}`))
       }
